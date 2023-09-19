c8c69f98f2b47151826505fe03424161d956afc1
Bigbo-tx <129650487+Bigbo-tx@users.noreply.github.com>
2023-09-19 06:41:38 +0530

    Create ping.py

---
+++ plugins/ping.py
@@ -0,0 +1,14 @@
+import time
+from pyrogram import Client, filters
+
+@bot.on_message(filters.private & filters.command("p2"))
+async def ping_command(client, message):
+    start_time = time.time()
+    sent_message = await message.reply("Pinging...")
+    end_time = time.time()
+    rtt = (end_time - start_time) * 50
+    await sent_message.edit(f"Pong! RTT: {rtt:.2f} ms")
+
+@bot.on_message(filters.private & filters.command("p2"))
+async def p2_command(client, message):
+    await p2_command(client, message)
