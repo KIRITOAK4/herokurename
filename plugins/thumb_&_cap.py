48cbd554e3cbd093b469e5cc4c2215db7078440f
Nakshtra Sharma <117059916+Nakshtra-Sharma@users.noreply.github.com>
2023-09-25 22:46:37 +0530

    Update thumb_&_cap.py

--- plugins/thumb_&_cap.py
+++ plugins/thumb_&_cap.py
@@ -1,7 +1,8 @@
 from pyrogram import Client, filters 
 from helper.database import db
+from Krito import pbot
 
-@Client.on_message(filters.private & filters.command('set_caption'))
+@pbot.on_message(filters.private & filters.command('set_caption'))
 async def add_caption(client, message):
     if len(message.command) == 1:
        return await message.reply_text("**__Gɪᴠᴇ Tʜᴇ Cᴀᴩᴛɪᴏɴ__\n\nExᴀᴍᴩʟᴇ:- `/set_caption {filename}\n\n💾 Sɪᴢᴇ: {filesize}\n\n⏰ Dᴜʀᴀᴛɪᴏɴ: {duration}`**")
@@ -9,7 +10,7 @@ async def add_caption(client, message):
     await db.set_caption(message.from_user.id, caption=caption)
     await message.reply_text("__**✅ Cᴀᴩᴛɪᴏɴ Sᴀᴠᴇᴅ**__")
    
-@Client.on_message(filters.private & filters.command('del_caption'))
+@pbot.on_message(filters.private & filters.command('del_caption'))
 async def delete_caption(client, message):
     caption = await db.get_caption(message.from_user.id)  
     if not caption:
@@ -17,7 +18,7 @@ async def delete_caption(client, message):
     await db.set_caption(message.from_user.id, caption=None)
     await message.reply_text("__**❌️ Cᴀᴩᴛɪᴏɴ Dᴇʟᴇᴛᴇᴅ**__")
                                        
-@Client.on_message(filters.private & filters.command(['see_caption', 'view_caption']))
+@pbot.on_message(filters.private & filters.command(['see_caption', 'view_caption']))
 async def see_caption(client, message):
     caption = await db.get_caption(message.from_user.id)  
     if caption:
@@ -26,7 +27,7 @@ async def see_caption(client, message):
        await message.reply_text("__**😔 Yᴏᴜ Dᴏɴ'ᴛ Hᴀᴠᴇ Aɴy Cᴀᴩᴛɪᴏɴ**__")
 
 
-@Client.on_message(filters.private & filters.command(['view_thumb', 'viewthumb']))
+@pbot.on_message(filters.private & filters.command(['view_thumb', 'viewthumb']))
 async def viewthumb(client, message):    
     thumb = await db.get_thumbnail(message.from_user.id)
     if thumb:
@@ -34,12 +35,12 @@ async def viewthumb(client, message):
     else:
         await message.reply_text("😔 __**Yᴏᴜ Dᴏɴ'ᴛ Hᴀᴠᴇ Aɴy Tʜᴜᴍʙɴᴀɪʟ**__") 
 		
-@Client.on_message(filters.private & filters.command(['del_thumb', 'delthumb']))
+@pbot.on_message(filters.private & filters.command(['del_thumb', 'delthumb']))
 async def removethumb(client, message):
     await db.set_thumbnail(message.from_user.id, file_id=None)
     await message.reply_text("❌️ __**Tʜᴜᴍʙɴᴀɪʟ Dᴇʟᴇᴛᴇᴅ**__")
 	
-@Client.on_message(filters.private & filters.photo)
+@pbot.on_message(filters.private & filters.photo)
 async def addthumbs(client, message):
     mkn = await message.reply_text("Please Wait ...")
     await db.set_thumbnail(message.from_user.id, file_id=message.photo.file_id)                
