2e00d6ccf830abc6549d284499d58957576dd077
Nakshtra Sharma <117059916+Nakshtra-Sharma@users.noreply.github.com>
2023-09-25 21:59:32 +0530

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -1,12 +1,12 @@
-# import time
-# from pyrogram import Client, filters
-# from make import ubot
+import time
+from pyrogram import Client, filters
+from bot import ubot
 
-# @ubot.on_message(filters.private & filters.command("p2"))
-# async def p2_command(client, message):
-#     print("Working")
-#     start_time = time.time()
-#     sent_message = await message.reply("Pinging...")
-#     end_time = time.time()
-#     rtt = (end_time - start_time) * 50
-#     await sent_message.edit(f"Pong! RTT: {rtt:.2f} ms")
+@ubot.on_message(filters.private & filters.command("p2"))
+async def p2_command(client, message):
+    print("Working")
+    start_time = time.time()
+    sent_message = await message.reply("Pinging...")
+    end_time = time.time()
+    rtt = (end_time - start_time) * 50
+    await sent_message.edit(f"Pong! RTT: {rtt:.2f} ms")
