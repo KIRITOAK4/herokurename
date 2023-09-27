1ec07e8df96ee64b7d6f7659af02e41e6d717f9c
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-09-27 23:46:30 +0530

    Update __main__.py

--- Krito/__main__.py
+++ Krito/__main__.py
@@ -8,8 +8,10 @@ import asyncio
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
 
+async def main():
+    await pbot.start()
+    await ubot.start()
+    await asyncio.gather(pbot.idle(), ubot.idle())
 
-pbot.start()
-ubot.start()
-loop = asyncio.get_event_loop()
-loop.run_forever()    
+if __name__ == "__main__":
+    asyncio.run(main())
