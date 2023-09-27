f34456c8401ce32a4b4a0afba99836cb7d8daa9f
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-09-27 22:00:01 +0530

    Update __init__.py

--- Krito/__init__.py
+++ Krito/__init__.py
@@ -6,7 +6,7 @@ import sys
 
 logging.basicConfig(level=logging.INFO)
 
-LOGS = logging.getLogger("AnimeBot")
+LOGS = logging.getLogger("RenameBot")
 LOGS.setLevel(level=logging.INFO)
 
 from datetime import datetime
@@ -23,11 +23,22 @@ import asyncio
 #-------------------------------USER----------------------------------------
 SESSION_STRING = os.environ.get("SESSION_STRING",Config.SESSION_STRING)
 #-------------------------------VARS-----------------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN",Config.BOT_TOKEN)
+ADMIN =  os.environ.get("ADMIN",Config.ADMIN)
 API_ID = int(os.environ.get("API_ID",Config.API_ID))
 API_HASH = os.environ.get("API_HASH",Config.API_HASH)
-OWNER = int(os.environ.get("OWNER",1636310615))
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL",-1001322925970))
+BOT_TOKEN = os.environ.get("BOT_TOKEN",Config.BOT_TOKEN)
+BOT_NAME = os.environ.get("BOT_NAME",Config.BOT_NAME)
+BOT_UPTIME = os.environ.get("BOT_UPTIME",Config.BOT_UPTIME)
+DB_NAME = os.environ.get("DB_NAME",Config.DB_NAME)
+DB_URL = os.environ.get("DB_URL",Config.DB_URL)
+FORCE_SUB = int(os.environ.get("FORCE_SUB",Config.FORCE_SUB)
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL",Config.LOG_CHANNEL)
+Text = os.environ.get("Text",Config.Text)
+Text1 = os.environ.get("Text1",Config.Text1)
+Text2 = os.environ.get("Text2",Config.Text2)
+Text3 = os.environ.get("Text3",Config.Text3)
+TOKEN_TIMEOUT = int(os.environ.get("TOKEN_TIMEOUT",Config.TOKEN_TIMEOUT))
+WEBHOOK = os.environ.get("WEBHOOK",Config.WEBHOOK) 
 #-------------------------------DEFAULT---------------------------------------
 TRIGGERS = os.environ.get("TRIGGERS", "/").split()
 UTRIGGERS = os.environ.get("TRIGGERS", ".").split()
