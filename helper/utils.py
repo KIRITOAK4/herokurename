f79ac69b6657f483604647c968af30dea42b2c21
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-09-29 16:28:49 +0530

    Update utils.py

--- helper/utils.py
+++ helper/utils.py
@@ -1,7 +1,8 @@
 import math, time
 from datetime import datetime
 from pytz import timezone
-from config import Config, Txt 
+from config import Txt 
+from Krito.__init__ import LOG_CHANNEL
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 async def progress_for_pyrogram(current, total, ud_type, message, start):
@@ -68,11 +69,11 @@ def convert(seconds):
     return "%d:%02d:%02d" % (hour, minutes, seconds)
 
 async def send_log(b, u):
-    if Config.LOG_CHANNEL is not None:
+    if LOG_CHANNEL is not None:
         curr = datetime.now(timezone("Asia/Kolkata"))
         date = curr.strftime('%d %B, %Y')
         time = curr.strftime('%I:%M:%S %p')
         await b.send_message(
-            Config.LOG_CHANNEL,
+            LOG_CHANNEL,
             f"**--Nᴇᴡ Uꜱᴇʀ Sᴛᴀʀᴛᴇᴅ Tʜᴇ Bᴏᴛ--**\n\nUꜱᴇʀ: {u.mention}\nIᴅ: `{u.id}`\nUɴ: @{u.username}\n\nDᴀᴛᴇ: {date}\nTɪᴍᴇ: {time}\n\nBy: {b.mention}"
         )
