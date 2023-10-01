a466918b11773c5d544442841a268d41c0a068b8
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-10-01 16:20:55 +0530

    Update __main__.py

--- Krito/__main__.py
+++ Krito/__main__.py
@@ -44,5 +44,6 @@ async def main():
         print(f"An error occurred during startup: {e}")
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(main())
+    asyncio.run(main())
+   #asyncio.get_event_loop().run_until_complete(main())
 
