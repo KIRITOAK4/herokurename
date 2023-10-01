b387ce135177a47567fbc4b73d2e181e680846d6
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-10-01 22:46:44 +0530

    Update __main__.py

--- Krito/__main__.py
+++ Krito/__main__.py
@@ -1,4 +1,4 @@
-
+import logging
 import asyncio
 from datetime import datetime
 from pytz import timezone
@@ -6,17 +6,43 @@ from aiohttp import web
 from Krito import pbot, create_ubot, WEBHOOK, BOT_UPTIME, ADMIN, LOG_CHANNEL
 from route import web_server
 from pyrogram import __version__
-
-pbot.start()
-
-success = create_ubot()  # Attempt to create ubot
-if success != "None":
-    ubot = success
-    ubot.start()
-try:
-  pbot.send_message(LOG_CHANNEL, text='Bot Started')
-except:
-    pass
-
-loop = asyncio.get_event_loop() 
-loop.run_forever()
+ 
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.INFO)  
+ 
+async def main():
+    try:
+        await pbot.start()
+ 
+        success = create_ubot()  # Attempt to create ubot
+        if success is not None:
+            ubot = success
+            await ubot.start()
+ 
+            me = await pbot.get_me()
+            print(f"{me.first_name} Is Started.....✨️")
+            if WEBHOOK:
+                app = web.AppRunner(await web_server())
+                await app.setup()       
+                await web.TCPSite(app, "0.0.0.0", 8080).start()     
+ 
+            for id in ADMIN:
+                try: 
+                    await pbot.send_message(id, f"**__{me.first_name} Iꜱ Sᴛᴀʀᴛᴇᴅ.....✨️__**")                                
+                except Exception as e:
+                    print(f"Failed to send message to admin {id}: {e}")
+ 
+            if LOG_CHANNEL:
+                try:
+                    curr = datetime.now(timezone("Asia/Kolkata"))
+                    date = curr.strftime('%d %B, %Y')
+                    time = curr.strftime('%I:%M:%S %p')
+                    await pbot.send_message(LOG_CHANNEL, f"**__{me.mention} Iꜱ Rᴇsᴛᴀʀᴛᴇᴅ !!**\n\n📅 Dᴀᴛᴇ : `{date}`\n⏰ Tɪᴍᴇ : `{time}`\n🌐 Tɪᴍᴇᴢᴏɴᴇ : `Asia/Kolkata`\n\n🉐 Vᴇʀsɪᴏɴ : `v{__version__}`</b>")
+                except Exception as e:
+                    print(f"Failed to send message to log channel: {e}")
+ 
+    except Exception as e:
+        print(f"An error occurred during startup: {e}")
+ 
+if __name__ == "__main__":
+    asyncio.run(main())
