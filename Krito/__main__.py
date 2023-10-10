279c61851d7957d78d38da1e772cd246356e2289
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-10-11 01:41:02 +0530

    Update __main__.py

--- Krito/__main__.py
+++ Krito/__main__.py
@@ -6,32 +6,33 @@ from aiohttp import web
 from Krito import pbot, create_ubot, WEBHOOK, BOT_UPTIME, ADMIN, LOG_CHANNEL
 from route import web_server
 from pyrogram import __version__
- 
+
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.INFO)  
- 
+logger = logging.getLogger(__name__)
+
 async def main():
     try:
         await pbot.start()
- 
+
         success = create_ubot()  # Attempt to create ubot
         if success is not None:
             ubot = success
             await ubot.start()
- 
+
             me = await pbot.get_me()
-            print(f"{me.first_name} Is Started.....✨️")
+            logger.info(f"{me.first_name} Is Started.....✨️")
             if WEBHOOK:
                 app = web.AppRunner(await web_server())
                 await app.setup()       
                 await web.TCPSite(app, "0.0.0.0", 8080).start()     
- 
+
             for id in ADMIN:
                 try: 
                     await pbot.send_message(id, f"**__{me.first_name} Iꜱ Sᴛᴀʀᴛᴇᴅ.....✨️__**")                                
                 except Exception as e:
-                    print(f"Failed to send message to admin {id}: {e}")
- 
+                    logger.error(f"Failed to send message to admin {id}: {e}")
+
             if LOG_CHANNEL:
                 try:
                     curr = datetime.now(timezone("Asia/Kolkata"))
@@ -39,10 +40,10 @@ async def main():
                     time = curr.strftime('%I:%M:%S %p')
                     await pbot.send_message(LOG_CHANNEL, f"**__{me.mention} Iꜱ Rᴇsᴛᴀʀᴛᴇᴅ !!**\n\n📅 Dᴀᴛᴇ : `{date}`\n⏰ Tɪᴍᴇ : `{time}`\n🌐 Tɪᴍᴇᴢᴏɴᴇ : `Asia/Kolkata`\n\n🉐 Vᴇʀsɪᴏɴ : `v{__version__}`</b>")
                 except Exception as e:
-                    print(f"Failed to send message to log channel: {e}")
- 
+                    logger.error(f"Failed to send message to log channel: {e}")
+
     except Exception as e:
-        print(f"An error occurred during startup: {e}")
- 
+        logger.error(f"An error occurred during startup: {e}")
+
 if __name__ == "__main__":
     asyncio.run(main())
