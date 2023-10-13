42a7764c831861f6c54109243b6935d350368c83
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-10-13 18:32:39 +0530

    Create extract.py

---
+++ helper/extract.py
@@ -0,0 +1,18 @@
+import re
+from Krito import Text, Text1, Text2, Text3
+
+def extract_message_id(link):
+    message_id_match = re.search(r'/(\d+)$', link)
+    if message_id_match:
+        return message_id_match.group(1)
+    else:
+        return None
+
+# Extract message IDs from the Text variables
+extracted_text = {
+    'Text': extract_message_id(Text),
+    'Text1': extract_message_id(Text1),
+    'Text2': extract_message_id(Text2),
+    'Text3': extract_message_id(Text3)
+}
+
