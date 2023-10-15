8553a5d0d8ddd3809a4a5bd3bfa8cc02f7aaa7e3
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-10-15 05:54:38 +0530

    Update __main__.py

--- Krito/__main__.py
+++ Krito/__main__.py
@@ -11,40 +11,7 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-async def main():
-    try:
-        await pbot.start()
-        await ubot.start()
-
-        while True:
-            me = await pbot.get_me()
-            logger.info(f"{me.first_name} Is Started.....✨️")
-            
-            if WEBHOOK:
-                app = web.AppRunner(await web_server())
-                await app.setup()
-                await web.TCPSite(app, "0.0.0.0", 8080).start()
-
-            for id in ADMIN:
-                try:
-                    await pbot.send_message(id, f"**__{me.first_name} Iꜱ Sᴛᴀʀᴛᴇᴅ.....✨️__**")
-                except Exception as e:
-                    logger.error(f"Failed to send message to admin {id}: {e}")
-
-            if LOG_CHANNEL:
-                try:
-                    curr = datetime.now(timezone("Asia/Kolkata"))
-                    date = curr.strftime('%d %B, %Y')
-                    time = curr.strftime('%I:%M:%S %p')
-                    await pbot.send_message(LOG_CHANNEL, f"**__{me.mention} Iꜱ Rᴇsᴛᴀʀᴛᴇᴅ !!**\n\n📅 Dᴀᴛᴇ : `{date}`\n⏰ Tɪᴍᴇ : `{time}`\n🌐 Tɪᴍᴇᴢᴏɴᴇ : `Asia/Kolkata`\n\n🉐 Vᴇʀsɪᴏɴ : `v{__version__}`</b>")
-                except Exception as e:
-                    logger.error(f"Failed to send message to log channel: {e}")
-                    
-            await asyncio.sleep(60)  # Sleep for 60 seconds before repeating
-
-    except Exception as error:
-        logger.error(f"An error occurred during startup: {error}")
-
-if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_forever()
+pbot.start()
+ubot.start()
+loop = asyncio.get_event_loop()
+loop.run_forever()    
