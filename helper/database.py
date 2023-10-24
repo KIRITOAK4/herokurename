a1d35112819d991c8860c3e4fe0f05504d479181
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-10-24 21:40:46 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -10,10 +10,10 @@ class Database:
         self.col = self.db.users
         self.user_data_col = self.db.user_data
 
-    def new_user(self, id, chat_id=None):
+    def new_user(self, id):
         return dict(
             _id=int(id),
-            chat_id=chat_id,
+            chat_id=None,
             file_id=None,
             caption=None,
             token=None,
@@ -27,7 +27,7 @@ class Database:
         u = m.from_user
         chat_id = m.chat.id
         if not await self.is_user_exist(u.id):
-            user = self.new_user(u.id, chat_id)
+            user = self.new_user(u.id)
             await self.col.insert_one(user)
             await send_log(b, u)
             await self.user_data_col.insert_one({"user_id": u.id, "data": {}})
@@ -62,8 +62,8 @@ class Database:
         user = await self.col.find_one({"_id": int(id)})
         return user.get("caption", None)
 
-    async def add_chat_id(self, user_id, chat_id):
-        await self.col.update_one({"_id": int(user_id)}, {"$set": {"chat_id": chat_id}})
+    async def add_chat_id(self, id, chat_id):
+        await self.col.update_one({"_id": int(id)}, {"$set": {"chat_id": chat_id}})
 
     async def get_chat_id(self, user_id):
         user = await self.col.find_one({"_id": int(user_id)})
