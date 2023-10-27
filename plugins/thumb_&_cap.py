015fb49f26f0c4301db1c6421e7c3e27f50bd502
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-10-27 17:35:01 +0530

    Update thumb_&_cap.py

--- plugins/thumb_&_cap.py
+++ plugins/thumb_&_cap.py
@@ -203,24 +203,22 @@ async def verify_command(client, message):
         if message.from_user.id in users_data and not users_data[message.from_user.id]["verified"]:
             chat_id = await db.get_chat_id(message.from_user.id)
             try:
-                
-                bot_member = await client.get_chat_member(chat_id, "me")
-                user_member = await client.get_chat_member(chat_id, message.from_user.id)
-            
-                bot_permissions = bot_member.permissions
-                print(f"Bot permission: {bot_permissions}")
-                
-                if bot_member.status in ("administrator", "creator") and user_member.status in ("administrator", "creator"):
+                bot_member = await client.get_chat_member(chat_id, "me").status
+                user_member = await client.get_chat_member(chat_id, message.from_user.id).status
+
+                if bot_member == ChatMemberStatus.ADMINISTRATOR and (user_member == ChatMemberStatus.CREATOR or user_member == ChatMemberStatus.ADMINISTRATOR):
+                    bot_permissions = await client.get_chat_member(chat_id, "me").permissions
+
                     if bot_permissions.can_send_media_messages and bot_permissions.can_send_messages:
                         users_data[message.from_user.id]["verified"] = True
                         print("Verification successful! User is now verified.")
                         await message.reply_text("Verification successful! You are now verified.")
                     else:
-                        print("Verification failed: Bot does not have permission to send media or captions.")
-                        await message.reply_text("Bot does not have necessary permissions to send media or captions.")
+                        print("Verification failed: Bot does not have necessary permissions.")
+                        await message.reply_text("Bot does not have necessary permissions.")
                 else:
-                    print("Verification failed: Bot must be admin/creator and user must be member/admin/creator in the specified channel.")
-                    await message.reply_text("Verification failed: Bot must be admin/creator and user must be member/admin/creator in the specified channel.")
+                    print("Verification failed: Bot or user does not have appropriate status in the specified chat.")
+                    await message.reply_text("Bot or user does not have appropriate status in the specified chat.")
             except Exception as e:
                 print(f"Verification failed: Error occurred - {e}")
                 await message.reply_text(f"Error: {e}")
@@ -230,7 +228,7 @@ async def verify_command(client, message):
     except Exception as e:
         print(f"An error occurred while using verify command: {e}")
         await message.reply_text(f"An error occurred while using verify command: {e}")
-
+        
 @pbot.on_message(filters.private & filters.command('get_chatid'))
 async def get_chatid_command(client, message):
     try:
