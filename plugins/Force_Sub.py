e58861eeb8d992b5c56401316ae0dc86b6a85e5a
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-11-02 18:57:34 +0530

    Update Force_Sub.py

--- plugins/Force_Sub.py
+++ plugins/Force_Sub.py
@@ -20,19 +20,24 @@ async def not_subscribed(_, client, message):
 
 @pbot.on_message(filters.private & filters.create(not_subscribed))
 async def forces_sub(client, message):
-    try:
-        invite_link = await client.export_chat_invite_link(FORCE_SUB)
-    except Exception as e:
-        print(f"Error: {e}")
-        return await message.reply_text("Failed to get the invite link. Please try again later.")
-    
-    buttons = [[InlineKeyboardButton(text="📢 Join Update Channel 📢", url=invite_link)]]
+    force_subs = FORCE_SUB.split()  # Split FORCE_SUB values by spaces
+    buttons = []
     text = "**Sᴏʀʀʏ Dᴜᴅᴇ Yᴏᴜ'ʀᴇ Nᴏᴛ Jᴏɪɴᴇᴅ My Cʜᴀɴɴᴇʟ 😐. Sᴏ Pʟᴇᴀꜱᴇ Jᴏɪɴ Oᴜʀ Uᴩᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ Tᴏ Cᴏɴᴛɪɴᴜᴇ**"
-    
+
+    for force_sub in force_subs:
+        try:
+            invite_link = await client.export_chat_invite_link(force_sub)
+            button = [InlineKeyboardButton(text=f"📢 Join Update Channel {force_sub} 📢", url=invite_link)]
+            buttons.extend(button)
+        except Exception as e:
+            print(f"Error: {e}")
+            return await message.reply_text("Failed to get the invite link. Please try again later.")
+
     try:
         user = await client.get_chat_member(FORCE_SUB, message.from_user.id)
         if user.status == enums.ChatMemberStatus.BANNED:
             return await client.send_message(message.from_user.id, text="Sᴏʀʀʏ Yᴏᴜ'ʀᴇ Bᴀɴɴᴇᴅ Tᴏ Uꜱᴇ Mᴇ")
     except UserNotParticipant:
-        return await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
-    return await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
+        return await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup([buttons]))
+
+    return await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup([buttons]))
