f469711f27e4c7185f86c972b47215e3d7dc232a
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-11-03 13:00:04 +0530

    Update Force_Sub.py

--- plugins/Force_Sub.py
+++ plugins/Force_Sub.py
@@ -9,22 +9,20 @@ async def not_subscribed(_, client, message):
     if not FORCE_SUB:
         return False
     try:
-        user = await client.get_chat_member(FORCE_SUB, message.from_user.id)
-        if user.status == enums.ChatMemberStatus.BANNED:
-            return True
-        else:
-            return False
+        for force_sub in FORCE_SUB:
+            user = await client.get_chat_member(force_sub, message.from_user.id)
+            if user.status != enums.ChatMemberStatus.BANNED:
+                return False
     except UserNotParticipant:
         pass
     return True
 
 @pbot.on_message(filters.private & filters.create(not_subscribed))
 async def forces_sub(client, message):
-    force_subs = FORCE_SUB.split()  # Split FORCE_SUB values by spaces
     buttons = []
     text = "**Sᴏʀʀʏ Dᴜᴅᴇ Yᴏᴜ'ʀᴇ Nᴏᴛ Jᴏɪɴᴇᴅ My Cʜᴀɴɴᴇʟ 😐. Sᴏ Pʟᴇᴀꜱᴇ Jᴏɪɴ Oᴜʀ Uᴩᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ Tᴏ Cᴏɴᴛɪɴᴜᴇ**"
 
-    for force_sub in force_subs:
+    for force_sub in FORCE_SUB:
         try:
             invite_link = await client.export_chat_invite_link(force_sub)
             button = [InlineKeyboardButton(text=f"📢 Join Update Channel {force_sub} 📢", url=invite_link)]
@@ -34,9 +32,10 @@ async def forces_sub(client, message):
             return await message.reply_text("Failed to get the invite link. Please try again later.")
 
     try:
-        user = await client.get_chat_member(FORCE_SUB, message.from_user.id)
-        if user.status == enums.ChatMemberStatus.BANNED:
-            return await client.send_message(message.from_user.id, text="Sᴏʀʀʏ Yᴏᴜ'ʀᴇ Bᴀɴɴᴇᴅ Tᴏ Uꜱᴇ Mᴇ")
+        for force_sub in FORCE_SUB:
+            user = await client.get_chat_member(force_sub, message.from_user.id)
+            if user.status == enums.ChatMemberStatus.BANNED:
+                return await client.send_message(message.from_user.id, text="Sᴏʀʀʏ Yᴏᴜ'ʀᴇ Bᴀɴɴᴇᴅ Tᴏ Uꜱᴇ Mᴇ")
     except UserNotParticipant:
         return await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup([buttons]))
 
