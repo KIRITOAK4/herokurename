36c3fa74e2eea1bbc4403025d68466bd314e252d
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-11-03 16:52:29 +0530

    Update Force_Sub.py

--- plugins/Force_Sub.py
+++ plugins/Force_Sub.py
@@ -32,13 +32,15 @@ async def forces_sub(client, message):
     except Exception as e:
         return await message.reply_text(text=f"An error occurred: {e}")
 
+    button_pairs = [buttons[i:i + 2] for i in range(0, len(buttons), 2)]
+    reply_markup = InlineKeyboardMarkup(button_pairs)    
+
     try:
         for force_sub in FORCE_SUB:
             user = await client.get_chat_member(force_sub, message.from_user.id)
             if user.status == enums.ChatMemberStatus.BANNED:
                 return await client.send_message(message.from_user.id, text="Sᴏʀʀʏ Yᴏᴜ'ʀᴇ Bᴀɴɴᴇᴅ Tᴏ Uꜱᴇ Mᴇ")
     except UserNotParticipant:
-        return await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup([buttons]))
+        return await message.reply_text(text=text, reply_markup=reply_markup)
 
-    reply_markup = InlineKeyboardMarkup([buttons[i:i + 2] for i in range(0, len(buttons), 2)])
     return await message.reply_text(text=text, reply_markup=reply_markup)
