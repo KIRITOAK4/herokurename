d554f364b4e06c4f0872e98430d2173aceb45dcc
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-11-04 05:33:28 +0530

    Update rename.py

--- plugins/rename.py
+++ plugins/rename.py
@@ -69,32 +69,41 @@ async def rename_start(client, message):
 
 @pbot.on_message(filters.private & filters.reply)
 async def refunc(client, message):
-    reply_message = message.reply_to_message
-    if (reply_message.reply_markup) and isinstance(reply_message.reply_markup, ForceReply):
-        new_name = message.text 
-        await message.delete() 
-        msg = await client.get_messages(message.chat.id, reply_message.id)
-        file = msg.reply_to_message
-        media = getattr(file, file.media.value)
-        if not "." in new_name:
-            if "." in media.file_name:
-                extn = media.file_name.rsplit('.', 1)[-1]
-            else:
-                extn = "mkv"
-            new_name = new_name + "." + extn
-        await reply_message.delete()
+    try:
+        user_id = message.from_user.id  # Extracting user ID 
+        on_cooldown, remaining_time = await process_and_update_cooldown(user_id)
+        if on_cooldown:
+            await message.reply_text(f"You are on cooldown. Please wait for {remaining_time} seconds.")
+            return
 
-        button = [[InlineKeyboardButton("📁 Dᴏᴄᴜᴍᴇɴᴛ",callback_data = "upload_document")]]
-        if file.media in [MessageMediaType.VIDEO, MessageMediaType.DOCUMENT]:
-            button.append([InlineKeyboardButton("🎥 Vɪᴅᴇᴏ", callback_data = "upload_video")])
-        elif file.media == MessageMediaType.AUDIO:
-            button.append([InlineKeyboardButton("🎵 Aᴜᴅɪᴏ", callback_data = "upload_audio")])
-        await message.reply(
-            text=f"**Sᴇʟᴇᴄᴛ Tʜᴇ Oᴜᴛᴩᴜᴛ Fɪʟᴇ Tyᴩᴇ**\n**• Fɪʟᴇ Nᴀᴍᴇ :-** `{new_name}`",
-            reply_to_message_id=file.id,
-            reply_markup=InlineKeyboardMarkup(button)
-        )
+        reply_message = message.reply_to_message
+        if (reply_message.reply_markup) and isinstance(reply_message.reply_markup, ForceReply):
+            new_name = message.text 
+            await message.delete() 
+            msg = await client.get_messages(message.chat.id, reply_message.id)
+            file = msg.reply_to_message
+            media = getattr(file, file.media.value)
+            if not "." in new_name:
+                if "." in media.file_name:
+                    extn = media.file_name.rsplit('.', 1)[-1]
+                else:
+                    extn = "mkv"
+                new_name = new_name + "." + extn
+            await reply_message.delete()
 
+            button = [[InlineKeyboardButton("📁 Dᴏᴄᴜᴍᴇɴᴛ", callback_data="upload_document")]]
+            if file.media in [MessageMediaType.VIDEO, MessageMediaType.DOCUMENT]:
+                button.append([InlineKeyboardButton("🎥 Vɪᴅᴇᴏ", callback_data="upload_video")])
+            elif file.media == MessageMediaType.AUDIO:
+                button.append([InlineKeyboardButton("🎵 Aᴜᴅɪᴏ", callback_data="upload_audio")])
+            await message.reply(
+                text=f"**Sᴇʟᴇᴄᴛ Tʜᴇ Oᴜᴛᴩᴜᴛ Fɪʟᴇ Tyᴩᴇ**\n**• Fɪʟᴇ Nᴀᴍᴇ :-** `{new_name}`",
+                reply_to_message_id=file.id,
+                reply_markup=InlineKeyboardMarkup(button)
+            )
+    except Exception as e:
+        error_text = f"An error occurred: {e}"
+        await message.reply_text(error_text)
 
 
 @pbot.on_callback_query(filters.regex("upload"))
