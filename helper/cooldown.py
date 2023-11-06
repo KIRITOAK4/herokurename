04646e5a17a202c73c2032828f425f0df4c1a2d1
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-11-06 17:41:33 +0530

    Update cooldown.py

--- helper/cooldown.py
+++ helper/cooldown.py
@@ -5,38 +5,39 @@ from Krito import MAX_PROCESS, COOLDOWN_DURATION
 completed_processes = {}
 cooldowns = {}
 
+print("Dictionaries for completed_processes and cooldowns initialized.")
+
 async def update_completed_processes(user_id):
-    if user_id in completed_processes:
-        completed_processes[user_id] += 1
-    else:
-        completed_processes[user_id] = 1
+    completed_processes[user_id] = completed_processes.get(user_id, 0) + 1
+    print(f"Completed processes for user {user_id} updated to {completed_processes[user_id]}.")
+    if completed_processes[user_id] >= MAX_PROCESS:
+        cooldowns[user_id] = time.time() + COOLDOWN_DURATION
+        print(f"User {user_id} is now on cooldown until {cooldowns[user_id]}.")
 
 async def check_cooldown(user_id):
-    if completed_processes.get(user_id, 0) >= MAX_PROCESS:
-        if user_id not in cooldowns:
-            cooldown_end_time = time.time() + COOLDOWN_DURATION
-            cooldowns[user_id] = cooldown_end_time
-        else:
-            cooldown_end_time = cooldowns[user_id]
-            current_time = time.time()
-            if current_time >= cooldown_end_time:
-                completed_processes[user_id] = 0
-                del cooldowns[user_id]
-                return False, 0
-            else:
-                remaining_time = int(cooldown_end_time - current_time)
-                return True, remaining_time
-    else:
-        return False, 0
+    print(f"Checking cooldown for user {user_id}.")
+    if user_id in cooldowns:
+        time_left = int(cooldowns[user_id] - time.time())
+        print(f"Time left for user {user_id}'s cooldown is {time_left}.")
+        if time_left <= 0:
+            completed_processes[user_id] = 0
+            del cooldowns[user_id]
+            print(f"Cooldown for user {user_id} has ended.")
+            return False, 0
+        print(f"User {user_id} is still on cooldown.")
+        return True, time_left
+    print(f"User {user_id} is not on cooldown.")
+    return False, 0
 
 async def process_and_update_cooldown(user_id):
-    try:
-        on_cooldown, remaining_time = await check_cooldown(user_id)
-        if on_cooldown:
-            return True, remaining_time
-        await update_completed_processes(user_id)
-        await db.update_user_cooldown_data(user_id)
-        return False, 0
-    except Exception as e:
-        print(f"Error in processing and updating cooldown: {e}")
-        return True, 0
+    print(f"Starting process and update cooldown for user {user_id}.")
+    on_cooldown, time_left = await check_cooldown(user_id)
+    if on_cooldown:
+        print(f"User {user_id} cannot proceed because they are on cooldown.")
+        return True, time_left
+    print(f"User {user_id} is not on cooldown. Proceeding with process update.")
+    await update_completed_processes(user_id)
+    print(f"Updating user {user_id} cooldown data in the database.")
+    await db.update_user_cooldown_data(user_id)
+    print(f"Process and cooldown update completed for user {user_id}.")
+    return False, 0
