3f166132217d60db491accdfc87211ecbd8b881a
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2023-11-08 18:09:29 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -30,16 +30,12 @@ async def restart_bot(b, m):
     await m.reply_text("🔄__Restarting...__")
     os.execl(sys.executable, sys.executable, *sys.argv)
 
-@pbot.on_message(filters.reply & filters.command("broadcast"))
+@pbot.on_message(filters.reply & filters.command("broadcast") & filters.user(ADMIN))
 async def broadcast_handler(bot: Client, m: Message):
-    print("Broadcast Command received")
-    if m.from_user.id not in ADMIN:
-        await m.reply_text("You are not authorized to use this command.", reply_to_message_id=m.id)
-        return
-    await bot.send_message(LOG_CHANNEL, f"{m.from_user.mention} or {m.from_user.id} has started the Broadcast......")
+    await bot.send_message(LOG_CHANNEL, f"{m.from_user.mention} or {m.from_user.id} Iꜱ ꜱᴛᴀʀᴛᴇᴅ ᴛʜᴇ Bʀᴏᴀᴅᴄᴀꜱᴛ......")
     all_users = await db.get_all_users()
     broadcast_msg = m.reply_to_message
-    sts_msg = await m.reply_text("Bʀᴏᴀᴅᴄᴀꜱᴛ Sᴛᴀʀᴛᴇᴅ.....!") 
+    sts_msg = await m.reply_text("Bʀᴏᴀᴅᴄᴀꜱᴛ Sᴛᴀʀᴛᴇᴅ..!") 
     done = 0
     failed = 0
     success = 0
@@ -48,17 +44,17 @@ async def broadcast_handler(bot: Client, m: Message):
     async for user in all_users:
         sts = await send_msg(user['_id'], broadcast_msg)
         if sts == 200:
-            success += 1
+           success += 1
         else:
-            failed += 1
+           failed += 1
         if sts == 400:
-            await db.delete_user(user['_id'])
-            done += 1
+           await db.delete_user(user['_id'])
+        done += 1
         if not done % 20:
-            await sts_msg.edit(f"Bʀᴏᴀᴅᴄᴀꜱᴛ Iɴ Pʀᴏɢʀᴇꜱꜱ: \nTᴏᴛᴀʟ Uꜱᴇʀꜱ {total_users} \nCᴏᴍᴩʟᴇᴛᴇᴅ: {done} / {total_users}\nSᴜᴄᴄᴇꜱꜱ: {success}\nFᴀɪʟᴇᴅ: {failed}")
+           await sts_msg.edit(f"Bʀᴏᴀᴅᴄᴀꜱᴛ Iɴ Pʀᴏɢʀᴇꜱꜱ: \nTᴏᴛᴀʟ Uꜱᴇʀꜱ {total_users} \nCᴏᴍᴩʟᴇᴛᴇᴅ: {done} / {total_users}\nSᴜᴄᴄᴇꜱꜱ: {success}\nFᴀɪʟᴇᴅ: {failed}")
     completed_in = datetime.timedelta(seconds=int(time.time() - start_time))
     await sts_msg.edit(f"Bʀᴏᴀᴅᴄᴀꜱᴛ Cᴏᴍᴩʟᴇᴛᴇᴅ: \nCᴏᴍᴩʟᴇᴛᴇᴅ Iɴ `{completed_in}`.\n\nTᴏᴛᴀʟ Uꜱᴇʀꜱ {total_users}\nCᴏᴍᴩʟᴇᴛᴇᴅ: {done} / {total_users}\nSᴜᴄᴄᴇꜱꜱ: {success}\nFᴀɪʟᴇᴅ: {failed}")
-                    
+
 async def send_msg(user_id, message):
     try:
         await message.copy(chat_id=int(user_id))
