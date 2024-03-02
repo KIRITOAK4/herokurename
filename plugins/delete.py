d5380a0ea219db5fd9f9bb18eecda785b4145154
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2024-03-02 17:32:10 +0530

    Update and rename thumb_&_cap.py to delete.py

--- plugins/delete.py
+++ plugins/delete.py
@@ -5,8 +5,7 @@ from Krito import pbot
 
 @pbot.on_message(filters.private & filters.command('del_caption'))
 async def delete_caption(client, message):
-    # Removed the unused none_admin_utils function
-
+    
     caption = await db.get_caption(message.from_user.id)
     if not caption:
         return await message.reply_text("__**😔 You Don't Have Any Caption**__")
@@ -16,8 +15,7 @@ async def delete_caption(client, message):
 
 @pbot.on_message(filters.private & filters.command(['see_caption', 'view_caption']))
 async def see_caption(client, message):
-    # Removed the unused none_admin_utils function
-
+    
     caption = await db.get_caption(message.from_user.id)
     if caption:
         await message.reply_text(f"**Your Caption:-**\n\n`{caption}`")
@@ -26,8 +24,16 @@ async def see_caption(client, message):
 
 @pbot.on_message(filters.private & filters.command(['del_thumb', 'delthumb']))
 async def removethumb(client, message):
-    # Removed the unused none_admin_utils function
-
+    
     await db.set_thumbnail(message.from_user.id, file_id=None)
     await message.reply_text("❌️ __**Thumbnail Deleted**__")
 
+@pbot.on_message(filters.private & filters.command('del_chat'))
+async def delete_caption(client, message):
+
+    chat_id = await db.get_chat_id(message.from_user.id)
+    if not chat_id:
+        return await message.reply_text("__**😔 You Don't Have Any Chat_id**__")
+
+    await db.set_chat_id(message.from_user.id, chat_id=None)
+    await message.reply_text("__**❌️ Chatid Deleted**__")
